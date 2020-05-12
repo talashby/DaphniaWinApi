4509697643b0d3ece2c3721091a5180f3224333a
Nikolay Ukhov <ukhov@mail.ru>
2020-05-12 21:23:42 +0300

    2.35 Send data about other observers to administrator observer.

--- DaphniaWinApi/ParallelPhysics/ObserverClient.h
+++ DaphniaWinApi/ParallelPhysics/ObserverClient.h
@@ -31,6 +31,8 @@ public:
 	void StopSimulation();
 	bool IsSimulationRunning() const;
 
+	uint64_t GetLastObserverID() const; // returns last observer ID successfully connected to server
+
 	void PPhTick();
 
 	SP_EyeColorArray GrabTexture();
@@ -63,6 +65,7 @@ protected:
 	bool SendServerMsg(const MsgBase &msg, int32_t msgSize); // returns false if error occur
 	virtual void HandleReceivedMessage(const char *buffer);
 
+private:
 	bool m_isSimulationRunning = false;
 
 	uint32_t m_socketC;
